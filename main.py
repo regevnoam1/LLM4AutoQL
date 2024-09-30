e5ed7b05a01b8cd8664620eae25840b0b77d5d82
regevnoam1 <regevnoam1@gmail.com>
2024-09-30 09:03:33 +0300

    ChatBot main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
+
+model = OllamaLLM(model="llama3")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def handle_converstaion():
+    context = ""
+    print("Welcome,this is an AI CHatBot for testing!")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+
+    res = chain.invoke({"context": context , "question": user_input})
+    print("Bot: ", res)
+    context += f"\nUser: {user_input}\nAI: {res}"
+
+if __name__=="__main__":
+    handle_converstaion()
